5182f7ee276b2e282ed0c5f6f68008d69cb3df15
AthaRazin <atharazin18@gmail.com>
2023-11-30 11:05:24 +0700

    tigabelas-commit

--- script/script.js
+++ script/script.js
@@ -83,7 +83,7 @@ const loginText = document.querySelector(".title-text .login");
 const loginForm = document.querySelector("form.login");
 const loginBtn = document.querySelector("label.login");
 const signupBtn = document.querySelector("label.signup");
-const signupLink = document.querySelector("form .signup-link a");
+const signupLink = document.querySelector("form.signup-link a");
 signupBtn.onclick = () => {
   loginForm.style.marginLeft = "-50%";
   loginText.style.marginLeft = "-50%";
@@ -110,3 +110,111 @@ function closeForm() {
 function btn_close() {
   document.getElementById("loginForm").style.display = "none";
 }
+
+// Script validate login
+function validateLoginForm() {
+  var username = document.getElementById("loginUsername").value;
+  var password = document.getElementById("loginPassword").value;
+
+  var usernameError = document.getElementById("loginUsernameError");
+  var passwordError = document.getElementById("loginPasswordError");
+  usernameError.innerHTML = "";
+  passwordError.innerHTML = "";
+
+  if (username.trim() === "") {
+    usernameError.innerHTML = "Username must be filled out";
+    return false;
+  }
+
+  if (password.trim().length < 6) {
+    passwordError.innerHTML = "Password must be at least 6 characters";
+    return false;
+  }
+
+  // Membuat objek data untuk dikirim ke backend
+  var data = {
+    username: username,
+    password: password,
+  };
+
+  fetch("http://localhost:3000/api/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((result) => {
+      console.log(result);
+      if (result.success) {
+        window.location.href = "index.html";
+      } else {
+        alert("Login failed. Please check your credentials.");
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+
+  return false;
+}
+// selesai
+
+// Script validate register
+function validateRegisterForm() {
+  var username = document.getElementById("registerUsername").value;
+  var password = document.getElementById("registerPassword").value;
+  var confirmPassword = document.getElementById("confirmPassword").value;
+
+  var usernameError = document.getElementById("registerUsernameError");
+  var passwordError = document.getElementById("registerPasswordError");
+  var confirmPasswordError = document.getElementById("confirmPasswordError");
+  usernameError.innerHTML = "";
+  passwordError.innerHTML = "";
+  confirmPasswordError.innerHTML = "";
+
+  if (username.trim() === "") {
+    usernameError.innerHTML = "Username must be filled out";
+    return false;
+  }
+
+  if (password.trim().length < 6) {
+    passwordError.innerHTML = "Password must be at least 6 characters";
+    return false;
+  }
+
+  if (password !== confirmPassword) {
+    confirmPasswordError.innerHTML = "Passwords do not match";
+    return false;
+  }
+
+  // Membuat objek data untuk dikirim ke backend
+  var data = {
+    username: username,
+    password: password,
+  };
+
+  fetch("http://localhost:3000/api/register", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((result) => {
+      console.log(result);
+      if (result.success) {
+        window.location.href = "index.html";
+      } else {
+        alert("Registration failed. Please try again.");
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+
+  return false;
+}
+// selesai
